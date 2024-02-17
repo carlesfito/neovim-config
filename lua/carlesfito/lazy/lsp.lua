1bff0cd7c0d5de7819b2ecc5517e4d8dc37a3e90
carlesplusplus <carles.fito.negre@gmail.com>
2024-02-17 17:42:50 +0100

    Adding lsp.

---
+++ lua/carlesfito/lazy/lsp.lua
@@ -0,0 +1,29 @@
+return {
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    -- {'williamboman/mason.nvim'},
+    -- {'williamboman/mason-lspconfig.nvim'},
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        {'williamboamn/mason.nvim'},
+        {'williamboamn/mason-lspconfig.nvim'},
+        {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+        {'neovim/nvim-lspconfig'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/nvim-cmp'},
+        {'L3MON4D3/LuaSnip'},
+    },
+    config = function ()
+        local lsp_zero = require('lsp-zero')
+
+        lsp_zero.on_attach(function(client, bufnr)
+            lsp_zero.default_keymaps({buffer = bufnr})
+        end)
+
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            handlers = {
+                lsp_zero.default_setup,
+            },
+        })
+    end
+}
