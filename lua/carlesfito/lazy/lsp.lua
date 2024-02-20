119f36f44888f7c1dc7f7a58f24061ac147c2f3b
Carles Fitó <cfito@feltor.com>
2024-02-20 11:00:39 +0100

    Add required mason servers.

--- lua/carlesfito/lazy/lsp.lua
+++ lua/carlesfito/lazy/lsp.lua
@@ -18,6 +18,7 @@ return {
 
         require('mason').setup({})
         require('mason-lspconfig').setup({
+            ensure_installed = {'tsserver', 'lua_ls'},
             handlers = {
                 lsp_zero.default_setup,
             },
