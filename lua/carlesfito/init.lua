ce78f4047f3c02ff2ef230259fa288fb05a9871f
Carles Fitó <cfito@feltor.com>
2024-08-28 15:52:41 +0200

    Add vim-cmp.

--- lua/carlesfito/init.lua
+++ lua/carlesfito/init.lua
@@ -1,5 +1,8 @@
 require("carlesfito.remap")
 require("carlesfito.settings")
+
 require("nvim-web-devicons").setup()
 require("nvim-web-devicons").get_icons()
 require("oil").setup()
+
+require("carlesfito.plugins.nvim-cmp")
